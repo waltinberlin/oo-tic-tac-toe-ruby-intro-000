b02b54b729e6918f941bec05888573ea2baba563
Walter Minehuber <minehuber@gmail.com>
2017-10-01 12:47:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -123,7 +123,7 @@ class TicTacToe
       turn
     end
     if winner != false
-      puts "Congratulations #{winner[0]}!"
+      puts "Congratulations #{@board[winner[0]]}!"
     else
     end
   #  if winner == false
